e7b1ba4455f86bd502a0e64f424b96d6b9704fd4
R-Tomasz <79597743+R-Tomasz@users.noreply.github.com>
2021-02-25 14:41:46 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="main-container">
+    <h1>Counter</h1>
+    <CounterApp />
+  </div>
+</template>
+
+<script>
+import CounterApp from "./components/CounterApp.vue";
+
+export default {
+  components: {
+    CounterApp,
+  },
+};
+</script>
